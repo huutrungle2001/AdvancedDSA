b044a80c20b3accb412a51c6c325c2f9965babb1
huutrungle2001 <huutrungle2001@gmail.com>
2021-09-03 20:57:39 +0700

    Change long long to int (easier to read code :v)

--- Segment Tree/Codeforces Practice/STforSum.cpp
+++ Segment Tree/Codeforces Practice/STforSum.cpp
@@ -1,32 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long getMid(long long start, long long end){
+int getMid(int start, int end){
     return start + (end - start)/2;
 }
 
-long long constructSTUntil(long long arr[], long long start, long long end, long long *st, long long current){
+int constructSTUntil(int arr[], int start, int end, int *st, int current){
     if(start == end){
         st[current] = arr[start];
         return arr[start];
     }
 
-    long long mid = getMid(start, end);
+    int mid = getMid(start, end);
     
     st[current] = constructSTUntil(arr, start, mid, st, current*2 + 1) 
                 + constructSTUntil(arr, mid + 1, end, st, current*2 + 2);
     return st[current];
 }
 
-long long *constructST(long long arr[], long long n){
-    long long x = (long long)(ceil(log2(n)));
-    long long max_size = 2*(long long)pow(2, x) - 1;
-    long long *st = new long long[max_size];
+int *constructST(int arr[], int n){
+    int x = (int)(ceil(log2(n)));
+    int max_size = 2*(int)pow(2, x) - 1;
+    int *st = new int[max_size];
     constructSTUntil(arr, 0, n - 1, st, 0);
     return st;
 }
 
-long long getSumUntil(long long *st, long long start, long long end, long long l, long long r, long long current){
+int getSumUntil(int *st, int start, int end, int l, int r, int current){
     if(l <= start && r >= end){
         return st[current];
     }
@@ -35,33 +35,33 @@ long long getSumUntil(long long *st, long long start, long long end, long long l
         return 0;
     }
 
-    long long mid = getMid(start, end);
+    int mid = getMid(start, end);
     return getSumUntil(st, start, mid, l, r, current*2 + 1) + getSumUntil(st, mid + 1, end, l, r, current*2 + 2);
 }
 
-long long getSum(long long *st, long long n, long long l, long long r){
+int getSum(int *st, int n, int l, int r){
     return (l < 0 || r > n - 1 || l > r) ? -1 : getSumUntil(st, 0, n - 1, l, r, 0);
 }
 
-void updateValueUntil(long long *st, long long start, long long end, long long i, long long diff, long long current){
+void updateValueUntil(int *st, int start, int end, int i, int diff, int current){
     if(i < start || i > end){
         return;
     }
     st[current] += diff;
 
     if(start != end) {
-        long long mid = getMid(start, end);
+        int mid = getMid(start, end);
         updateValueUntil(st, start, mid, i, diff, current*2 + 1);
         updateValueUntil(st, mid + 1, end, i, diff, current*2 + 2);
     }
 }
 
-void updateValue(long long arr[], long long *st, long long n, long long i, long long value){
+void updateValue(int arr[], int *st, int n, int i, int value){
     if(i < 0 || i > n) {
         return;
     }
 
-    long long diff = value - arr[i];
+    int diff = value - arr[i];
     arr[i] = value;
 
     updateValueUntil(st, 0, n - 1, i, diff, 0);
@@ -69,25 +69,25 @@ void updateValue(long long arr[], long long *st, long long n, long long i, long 
 
 
 int main(){
-    long long n, m, option, index, value, left, right;
+    int n, m, option, index, value, left, right;
     cin >> n >> m;
-    long long arr[n];
-    for(long long i = 0; i < n; i++){
+    int arr[n];
+    for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
 
-    long long *st = constructST(arr, n);
+    int *st = constructST(arr, n);
 
-    for(long long i = 0; i < m; i++){
+    for(int i = 0; i < m; i++){
         cin >> option;
-        switch(option){
+        switch(option){ 
             case 1: 
                 cin >> index >> value;
                 updateValue(arr, st, n, index, value);
                 break;
             case 2: 
                 cin >> left >> right;
-                cout << getSum(st, n, left, right - left - 1) << endl;
+                cout << getSum(st, n, left, right - 1) << endl;
                 break;
         }
     }
